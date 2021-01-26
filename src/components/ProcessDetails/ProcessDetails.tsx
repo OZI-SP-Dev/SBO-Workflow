d998a5f382ba5de1a87d2bdeaa4b153411997b30
clarkiness72 <60795431+clarkiness72@users.noreply.github.com>
2021-01-26 07:40:36 -0500

    resized the detail cols

--- src/components/ProcessDetails/ProcessDetails.tsx
+++ src/components/ProcessDetails/ProcessDetails.tsx
@@ -12,82 +12,82 @@ export const ProcessDetails: FunctionComponent<IProcessDetailsProps> = (props) =
     return (
         <div className="process-details">
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Solicitation/Contract Number</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.SolicitationNumber}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Program Name or Item Being Acquired</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.ProgramName}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Buyer's Organization</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.Org}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Buyer</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.Buyer.Title}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Contracting Officer</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.ContractingOfficer.Title}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Small Business Professional</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.SmallBusinessProfessional.Title}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>SBA PCR</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.SBAPCR?.Title}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>PoP</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.SboDuration}
                 </Col>
             </Row>
             <Row className="mb-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Total Contract Value</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.ContractValueDollars}
                 </Col>
             </Row>
             {props.process.ProcessType === ProcessTypes.DD2579 && <Row>
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
+                <Col md="5" sm="12" xs="12">
                     <strong>Set-Aside Recommendation</strong>
                 </Col>
-                <Col xl="7" lg="7" md="7" sm="7" xs="7">
+                <Col md="7" sm="12" xs="12">
                     {props.process.SetAsideRecommendation}
                 </Col>
             </Row>}
