1c90e7dd4e373a377def38de87fd2e8ba25032f6
clarkiness72 <60795431+clarkiness72@users.noreply.github.com>
2021-02-02 14:56:03 -0500

    react-html-parser to domPurify

--- src/components/NotesView/NoteView.tsx
+++ src/components/NotesView/NoteView.tsx
@@ -2,7 +2,7 @@ import { DateTime } from "luxon";
 import React, { FunctionComponent } from "react";
 import { Card, Col } from "react-bootstrap";
 import { INote } from "../../api/DomainObjects";
-import ReactHtmlParser from 'react-html-parser';
+import DOMPurify from 'dompurify';
 
 
 export interface INoteViewProps {
@@ -14,9 +14,7 @@ export const NoteView: FunctionComponent<INoteViewProps> = (props) => {
     return (
         <Card className="note">
             <Card.Body as={Col} className="p-2">
-                <p className="preserve-whitespace mb-0">
-                    {ReactHtmlParser(props.note.Text)}<br />
-                </p>
+                <p className="preserve-whitespace mb-0" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.note.Text) }} />
                 <p className="note-timestamp float-right mb-0">
                     -<i>{props.note.Author.Title} at {props.note.Modified.toLocaleString(DateTime.DATETIME_MED)}</i>
                 </p>
