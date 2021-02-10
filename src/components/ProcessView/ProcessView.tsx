5c8520b4a66e257b52bd23724eb2efbce9089c8b
clarkiness72 <60795431+clarkiness72@users.noreply.github.com>
2021-02-10 10:53:46 -0500

    Actions menu on details page

--- src/components/ProcessView/ProcessView.tsx
+++ src/components/ProcessView/ProcessView.tsx
@@ -1,5 +1,6 @@
+import { Icon, IconButton } from "@fluentui/react";
 import React, { FunctionComponent, useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import { getBlankProcess, IProcess, ProcessTypes } from "../../api/DomainObjects";
 import { useProcessDetails } from "../../hooks/useProcessDetails";
 import { DocumentsView } from "../DocumentsView/DocumentsView";
@@ -8,6 +9,7 @@ import { NotesView } from "../NotesView/NotesView";
 import { ProcessDetails } from "../ProcessDetails/ProcessDetails";
 import SBOSpinner from "../SBOSpinner/SBOSpinner";
 import { StatusWorkflow } from "../StatusWorkflow/StatusWorkflow";
+import "./ProcessView.css"
 
 export interface IProcessViewProps {
     processId: number,
@@ -27,21 +29,37 @@ export const ProcessView: FunctionComponent<IProcessViewProps> = (props) => {
     }, [processDetails.loading]);
 
     return (
-        <Col xl="11" className="m-auto">
-            <HeaderBreadcrumbs crumbs={[{ crumbName: "Home", href: "#/" }, { crumbName: process ? process.SolicitationNumber : '' }]} />
-            <StatusWorkflow className="mt-3" process={process ? process : getBlankProcess(ProcessTypes.DD2579)} />
-            <Row className="mt-3 ml-2">
-                <Col xl="5" lg="5" md="5" sm="5" xs="5">
-                    <ProcessDetails
-                        process={process ? process : getBlankProcess(ProcessTypes.DD2579)}
-                    />
-                    <NotesView className="mt-5" notes={processDetails.notes} submitNote={processDetails.submitNote} />
+        <>
+            <Row className="m-0">
+                <Col xs="11" className="m-auto">
+                    <HeaderBreadcrumbs crumbs={[{ crumbName: "Home", href: "#/" }, { crumbName: process ? process.SolicitationNumber : '' }]} />
                 </Col>
-                <Col>
-                    <DocumentsView documents={processDetails.documents} loading={processDetails.loading} submitDocument={processDetails.submitDocument} deleteDocument={processDetails.deleteDocument} />
+                <Col xs="11" className="m-auto sbo-details-actions-col">
+                    <Card className="mt-3 p-2 sbo-gray-gradiant sbo-details-actions-card">
+                        <Row className="m-0 mr-auto">
+                            <Col className="m-0 p-0 m-auto orange" xl='12' xs='2'><Icon iconName="NavigateForward" className="sbo-details-icon" /></Col>
+                            <Col className="m-0 p-0 m-auto orange" xl='12' xs='2'><Icon iconName="NavigateBack" className="sbo-details-icon" /></Col>
+                            <Col className="m-0 p-0 m-auto blue" xl='12' xs='2'><Icon iconName="Edit" className="sbo-details-icon" /></Col>
+                            <Col className="m-0 p-0 m-auto red" xl='12' xs='2'><Icon iconName="Delete" className="sbo-details-icon" /></Col>
+                        </Row>
+                    </Card>
                 </Col>
             </Row>
-            <SBOSpinner show={processDetails.loading} displayText="Loading Process..." />
-        </Col>
+            <Col xs="11" className="m-auto">
+                <StatusWorkflow className="mt-3" process={process ? process : getBlankProcess(ProcessTypes.DD2579)} />
+                <Row className="mt-3 ml-2">
+                    <Col xl="5" lg="5" md="12" sm="12" xs="12">
+                        <ProcessDetails
+                            process={process ? process : getBlankProcess(ProcessTypes.DD2579)}
+                        />
+                        <NotesView className="mt-5" notes={processDetails.notes} submitNote={processDetails.submitNote} />
+                    </Col>
+                    <Col>
+                        <DocumentsView documents={processDetails.documents} loading={processDetails.loading} submitDocument={processDetails.submitDocument} deleteDocument={processDetails.deleteDocument} />
+                    </Col>
+                </Row>
+                <SBOSpinner show={processDetails.loading} displayText="Loading Process..." />
+            </Col>
+        </>
     );
 }
