46fe2b8d68c1b9a8c580d9fd382cf4fe508c6d3e
clarkiness72 <60795431+clarkiness72@users.noreply.github.com>
2021-06-22 09:36:47 -0400

    Update RouteParamRedirect.tsx

--- src/components/RouteParamRedirect/RouteParamRedirect.tsx
+++ src/components/RouteParamRedirect/RouteParamRedirect.tsx
@@ -7,10 +7,8 @@ export const RouteParamRedirect: FunctionComponent = () => {
     const routeParam = params.get("route");
 
     useEffect(() => {
-        console.log("out");
         if (routeParam) { // decode the URL encoding as the browser doesn't seem to do it for us
-            console.log("in");
-            window.location.replace(`${window.location.origin}${window.location.pathname}${routeParam.replace("%23", "#").replaceAll("%2F", "/")}`);
+            window.location.replace(`${window.location.origin}${window.location.pathname}${decodeURIComponent(routeParam)}`);
         }
     }, [routeParam]);
 
