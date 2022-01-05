b7ce90368359ebc51dc9e60d9686e81fd4fe648a
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-01-05 09:00:08 -0500

    Enable UAT banner and ability to start in npm

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -8,12 +8,12 @@ export const AppHeader: FunctionComponent = () => {
 
     return (
 
-        <Navbar id="mainnav" className="p-0 m-0">
-            <Navbar.Brand className={(process.env.REACT_APP_TEST_SYS ? "test mr-0 pl-1 pr-3" : "mr-0 pl-1 pr-3")}>
+        <Navbar id="mainnav" className="p-0 m-0" bg={(process.env.REACT_APP_TEST_SYS ? "danger" : "")}>
+            <Navbar.Brand className="mr-0 pl-1 pr-3">
                 <LinkContainer isActive={m => m !== null && m?.isExact} to="/">
                     <Nav.Link className="p-0">
                         <img src="./orangeeagle2.png" alt="SBO Logo" height="30px" />
-                        <span> Kolab</span>
+                        <span>{(process.env.REACT_APP_TEST_SYS ? " TEST" : "")} Kolab</span>
                     </Nav.Link>
                 </LinkContainer>
             </Navbar.Brand>
